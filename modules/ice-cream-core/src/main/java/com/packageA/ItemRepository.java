5bd3a2154b9dbf5f3b2a24f6ac623511779f4c0c
Rajeevadhr <rajeevadhr@gmail.com>
2018-03-16 10:04:19 +0530

    Changed crud repository to jpa repository

--- modules/ice-cream-core/src/main/java/com/packageA/ItemRepository.java
+++ modules/ice-cream-core/src/main/java/com/packageA/ItemRepository.java
@@ -1,11 +1,12 @@
 package com.packageA;
 
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-public interface ItemRepository extends CrudRepository<Item, Long> {
+public interface ItemRepository extends JpaRepository<Item, Long> {
     /**
      * Search an item using its name
      * @param itemName
